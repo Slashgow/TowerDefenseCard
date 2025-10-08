9d3fdb08e647900a5e756c2aba6e02e221acb7e6
William <william.plank@outlook.fr>
2025-10-08 10:48:12 +0200

    complete text ui difficulty

--- TowerDefenseCard/Assets/Scripts/Difficulty/UIDifficulty.cs
+++ TowerDefenseCard/Assets/Scripts/Difficulty/UIDifficulty.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class UIDifficulty : MonoBehaviour, IUISelectable<DifficultyData>
@@ -9,8 +10,33 @@ public class UIDifficulty : MonoBehaviour, IUISelectable<DifficultyData>
     [SerializeField] private DifficultyData difficultyData;
     public DifficultyData DifficultyData => difficultyData;
 
+    [SerializeField] private TextMeshProUGUI timeBetweenWaveModifierValueText;
+    [SerializeField] private TextMeshProUGUI ennemyHealthModifierValueText;
+    [SerializeField] private TextMeshProUGUI ennemyDamageModifierValueText;
+    [SerializeField] private TextMeshProUGUI ennemyAttackSpeedModifierValueText;
+
     public event Action<DifficultyData> OnSelectEvent;
 
     public DifficultyData GetSelectableData() => difficultyData;
     public void OnSelect(DifficultyData data) => OnSelectEvent?.Invoke(difficultyData);
+
+    private void Awake()
+    {
+
+        float modifier = difficultyData.TimeBetweenWavesPercentModifier;
+        string sign = modifier >= 0 ? "+" : "-";
+        timeBetweenWaveModifierValueText.text = $"{sign}{Mathf.Abs(modifier)}%";
+
+        modifier = difficultyData.EnnemyHealthPercentModifier;
+        sign = modifier >= 0 ? "+" : "-";
+        ennemyHealthModifierValueText.text = $"{sign}{Mathf.Abs(modifier)}%";
+
+        modifier = difficultyData.EnnemyDamagePercentModifier;
+        sign = modifier >= 0 ? "+" : "-";
+        ennemyDamageModifierValueText.text = $"{sign}{Mathf.Abs(modifier)}%";
+
+        modifier = difficultyData.EnnemyAttackSpeedPercentModifier;
+        sign = modifier >= 0 ? "+" : "-";
+        ennemyAttackSpeedModifierValueText.text = $"{sign}{Mathf.Abs(modifier)}%";
+    }
 }
